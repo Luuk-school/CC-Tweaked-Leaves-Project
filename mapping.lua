28a1355e046a19fa91c1fdd0f239b8348ba02f26
Luuk <551367@student.fontys.nl>
2026-05-04 16:46:01 +0200

    mapping done return object list to runtime

--- mapping.lua
+++ mapping.lua
@@ -1,45 +1,53 @@
--- block readers functie
-local function blockReaders()
-    for_, name in ipairs(peripheral.getNames()) do
-        if name:match("^block_reader_(%d+)$") then
-            local reader = peripheral.wrap(name)
-            if reader then
-                print("block" .. reader)
-                if read == "minecraft:piston_head" then
-                    print("piston head detected")
-                    return name
+-- discovery
+local relay = {}
+local reader = {}
+local matchingList = {}
+
+for_, name in ipairs(peripheral.getNames()) do
+    if name:match("^redstone_relay_(%d+)$") then
+        table.insert(relay, name)
+    elseif name:match("^block_reader_(%d+)$") then
+        table.insert(reader, name)
+    end
+end
+
+-- matching      eerst 1 relay aanzetten en all reader kijken
+for i = 1, #relay do
+    local relays = peripheral.wrap(relay[i])
+    relays.setOutput("front", true)
+    for j = 1, #reader do
+        local readers = peripheral.wrap(reader[j])
+        local matched = false
+        local maxRetries = 3
+        local retries = 0
+        
+        while not matched and retries < maxRetries do
+            if readers.getBlock() == "minecraft:piston_head" then
+                table.insert(matchingList, {relay = relay[i], reader = reader[j]})
+                matched = true
+            else
+                retries = retries + 1
+                if retries < maxRetries then
+                    sleep(0.5)
                 end
             end
         end
     end
+    relays.setOutput("front", false)
 end
 
-x = 0   -- tijdelijke x
--- mapping functie
-local function matching(relay)
-    local relay = peripheral.wrap(relay)
-    relay.setOutput("front", true)
-    local blockReader = blockReaders()
---    print(blockReader)
-    if blockReader then
-        x = x + 1
-        print(x)
-    end
-    relay.setOutput("front", false)
-end
+-- objectlist naar runtime
+object = matchingList
+return object
 
--- main loop
-local relays = {}
-local readers = {}
-for _, name in ipairs(peripheral.getNames()) do
-    if name:match("^redstone_relay_(%d+)$") then
-        matching(name)
-    end
-end
 
---mapping functie
-local devicePairs = {}
-for i = 1, math.min(#relays, #readers) do
-    devicePairs[#devicePairs + 1] = {relay = relays[i], reader = readers[i]}
-end
 
+
+--          todo lijst.
+-- 1 discover                   done
+-- 2 matching                   done
+-- 2.1 wait for functie maken   done
+-- 2.3 procces paring (list)    done
+-- 3 testing                    skiped
+-- 3 list naar main sturen      done
+-- 4 monitor maken              ???
