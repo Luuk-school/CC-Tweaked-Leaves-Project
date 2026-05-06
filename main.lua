5456724a162dbf0f301d8e0c44e33bdfe49cf3ef
Luuk <551367@student.fontys.nl>
2026-05-06 18:01:25 +0200

    Main uitgewerkt;
    Hiernaast zijn er nog placeholders, metadata van de blok moet eerst gekeken worden in minecraft.

--- main.lua
+++ main.lua
@@ -1,1 +1,20 @@
 -- main file
+local object = require("mapping") --werkt mogelijk niet in CC: ander gebruik dofile
+--local object = dofile("mapping.lua") -- werkt mogelijk niet in CC: ander gebruik require
+                            --Verwijder later de commentaar.
+while true do
+    for i = 1, #object do
+        local relay = peripheral.wrap(object[i].relay)
+        local reader = peripheral.wrap(object[i].reader)
+        
+        if reader.getBlock() == " Hier  moet je nog de blockdata van die leave neerzetten  " then
+            local stage = reader.getblockdata(stage)
+                if stage == 3 then
+                    relay.setOutput("front", true)
+                end
+        else
+            relay.setOutput("front", false)
+        end
+    end
+    sleep(30)
+ends
